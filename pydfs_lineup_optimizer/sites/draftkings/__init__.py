20dd39dfb738b8eb114c012a1d455c201da60400
Benika <benika.hall@gmail.com>
2020-07-10 15:40:56 -0400

    Updating __init__.py to add the DK Showdown

--- pydfs_lineup_optimizer/sites/draftkings/__init__.py
+++ pydfs_lineup_optimizer/sites/draftkings/__init__.py
@@ -2,11 +2,13 @@ from pydfs_lineup_optimizer.sites.draftkings.classic.importer import DraftKingsC
 from pydfs_lineup_optimizer.sites.draftkings.classic.settings import *
 from pydfs_lineup_optimizer.sites.draftkings.captain_mode.importer import DraftKingsCaptainModeCSVImporter
 from pydfs_lineup_optimizer.sites.draftkings.captain_mode.settings import *
+from pydfs_lineup_optimizer.sites.draftkings.showdown.importer import DraftKingsShowdownGolfModeCSVImporter
+from pydfs_lineup_optimizer.sites.draftkings.showdown.settings import *
 
 __all__ = [
     'DraftKingsCSVImporter', 'DraftKingsBasketballSettings', 'DraftKingsFootballSettings',
     'DraftKingsHockeySettings', 'DraftKingsBaseballSettings', 'DraftKingsGolfSettings',
     'DraftKingsSoccerSettings', 'DraftKingsCanadianFootballSettings', 'DraftKingsLOLSettings',
     'DraftKingsCaptainModeCSVImporter', 'DraftKingsCaptainModeFootballSettings',
-    'DraftKingsCaptainModeBasketballSettings',
+    'DraftKingsCaptainModeBasketballSettings','DraftKingsShowdownGolfModeCSVImporter',
 ]
